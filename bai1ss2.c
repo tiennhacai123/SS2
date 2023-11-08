4d3ebd974172d09c5f5d8152d2ff1300a201b889
Tiến LD <147360077+tiennhacai123@users.noreply.github.com>
2023-11-08 08:17:17 +0700

    Add files via upload

--- bai1ss2.c
+++ bai1ss2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(){
     //khai bao va khoi tao canh hinh vuong, chieu dai chieu rong hinh chu nhat, ban kinh duong tron 
-    float square_edge = 'a', rectangular_width = 'b', rectangular_length = 'c', circle_radius = 'r';
+    float square_edge, rectangular_width, rectangular_length, circle_radius;
     const float PI = 3.14;
-    float square_perimeter = 'a' * 4;
-    float area_square = 'a' * 'a';
-    float perimeter_of_a_rectangle = ('b' + 'c') / 2;
-    float rectangular_area = 'b' * 'c';
-    float diameter_of_a_circle = 'r' * 2 * 3.14;
-    float circle_area = 3.14 * 'r' * 'r';
+    float square_perimeter = 'square_edge' * 4;
+    float area_square = 'square_edge' * 'square_edge';
+    float perimeter_of_a_rectangle = ('rectangular_width' + 'rectangular_length') * 2;
+    float rectangular_area = 'rectangular_width' * 'c';
+    float diameter_of_a_circle = 'circle_radius' * 2 * 3.14;
+    float circle_area = 3.14 * 'circle_radius' * 'circle_radius';
 }
